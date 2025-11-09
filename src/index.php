f076a6f07a373ff3025a6c734a79ffb80d6e39f2
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2025-11-09 13:31:56 +0100

    Fix basic css styling

--- src/index.php
+++ src/index.php
@@ -6,62 +6,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini Darts</title>
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=s" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/darts.css">
 </head>
-<body>
-    <section onmousemove="coordinate(event)">
-        <div class="menu">
-            <div class="menu-settings">
-                <div class="menu-setting" onclick="help()">Help</div>
-                <div class="menu-setting" onclick="fullscreen()">Fullscreen</div>
-            </div>
-            <div class="menu-settings">
-                <div class="menu-setting" onclick="scoreMode()">301</div>
-                <div class="menu-setting" onclick="playerMode()">Player(s)</div>
-                <div class="menu-setting" onclick="gameMode()">Single Out</div>
-            </div>  
-            <div class="help-info">
-                <ol>
-                    <li>Press and hold the left mouse button to take a dart.</li>
-                    <li>Swipe up the mouse and release the left mouse button to throw the dart.</li>
-                </ol> 
-                <br>Tip: The dart will fly higher, if you swipe fast. Swiping slowly may cause the dart to drop.
-            </div>
-            <p>Round</p>
-            <div class="infos">1</div>
-            <p>Score</p>
-            <div class="infos">
-                <div class="info-score">301</div>
-                <div class="info-score" style="display:none">301</div>
-                <div class="info-score" style="display:none">301</div>
-            </div>
-            <p>Counter</p>
-            <div class="info-counter infos"><div class="spanner">--</div> <div class="spanner">--</div> <div class="spanner">--</div> <div class="spanner">(--)</div></div>
-            <p>Average</p>
-            <div class="infos">
-                <div class="info-average">--</div>
-                <div class="info-average" style="display:none">--</div>
-                <div class="info-average" style="display:none">--</div>
-            </div>
-            <div class="darts">
-                <div class="arrow-img"></div>
-                <div class="arrow-img"></div>
-                <div class="arrow-img"></div>
-            </div>
-            <div class="restart-button" onclick="restartGame()">Restart</div>
+<body onmousemove="coordinate(event)">
+    <div id="menu">
+        <div class="menu-settings">
+            <div class="menu-setting" onclick="help()">Help</div>
+            <div class="menu-setting" onclick="fullscreen()">Fullscreen</div>
         </div>
-        <div class="dartboard" onmousedown="dartDown()" onmouseup="dartUp()">
-            <div class="game-comment">Player 1</div>
-            <div class="board">
-                <div class="flying-dart"></div>
-                <div class="flying-dart"></div>
-                <div class="flying-dart"></div>
-            </div>
-            <div class="cursor"></div>
+        <div class="menu-settings">
+            <div class="menu-setting" onclick="scoreMode()">301</div>
+            <div class="menu-setting" onclick="playerMode()">Player(s)</div>
+            <div class="menu-setting" onclick="gameMode()">Single Out</div>
+        </div>  
+        <div id="help-info">
+            <ol>
+                <li>Press and hold the left mouse button to take a dart.</li>
+                <li>Swipe up the mouse and release the left mouse button to throw the dart.</li>
+            </ol> 
+            <br>Tip: The dart will fly higher, if you swipe fast. Swiping slowly may cause the dart to drop.
         </div>
-    </section>
+        <p>Round</p>
+        <div class="scores">1</div>
+        <p>Score</p>
+        <div class="scores" id="score-overall">
+            <div class="score">301</div>
+            <div class="score" style="display:none">301</div>
+            <div class="score" style="display:none">301</div>
+        </div>
+        <p>Counter</p>
+        <div class="scores" id="score-round">
+            <span>--</span>
+            <span>--</span>
+            <span>--</span>
+            <span>(--)</span>
+        </div>
+        <p>Average</p>
+        <div class="scores">
+            <div class="score">--</div>
+            <div class="score" style="display:none">--</div>
+            <div class="score" style="display:none">--</div>
+        </div>
+        <div id="darts">
+            <div class="dart"></div>
+            <div class="dart"></div>
+            <div class="dart"></div>
+        </div>
+        <div id="restart" onclick="restartGame()">Restart</div>
+    </div>
+    <div id="playzone" onmousedown="dartDown()" onmouseup="dartUp()">
+        <div id="active-player">Player 1</div>
+        <div id="dartboard">
+            <div class="flying-dart"></div>
+            <div class="flying-dart"></div>
+            <div class="flying-dart"></div>
+        </div>
+        <div id="cursor"></div>
+    </div>
     <script src="js/darts.js"></script>
-    
 </body>
 </html>
